8ebc6ab03eea8b99cbd8b03d5713964f1561fc05
ngauerh <229827522@qq.com>
2018-11-21 23:42:07 +0800

    用户发帖

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.base import View
 from django.contrib.auth.decorators import login_required
+import json
+from .models import *
 from .forms import *
 # Create your views here.
 
@@ -10,7 +13,6 @@ def index(requests):
 
 
 # 发帖
-
 class NewTopic(View):
     @staticmethod
     @login_required
@@ -20,7 +22,19 @@ class NewTopic(View):
     @staticmethod
     @login_required
     def post(request):
-        form = TopicForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return render(request, 'topic/new_topic.html')
+        # print(request.POST['detail'])
+        print(request.POST['title'])
+        res = {"success": True, "msg": "成功"}
+        return HttpResponse(json.dumps(res), content_type="application/json")
+        # form = TopicForm(request.POST)
+        # if form.is_valid():
+        #     form.save()
+        #     res = {"success": True, "msg": "成功"}
+        #     return HttpResponse(json.dumps(res), content_type="application/json")
+        # return render(request, 'topic/new_topic.html')
+
+
+# 节点
+def boardlist(request):
+    board = Board.objects.all()
+    print(board)
