12e991edc34bd30439777cbc32f26a987a3c468e
ngauerh <229827522@qq.com>
2019-05-22 11:37:22 +0800

    templatetags

---
+++ apps/forum/templatetags/shortnaturaltime.py
@@ -0,0 +1,18 @@
+from django import template
+import datetime
+register = template.Library()
+
+
+@register.filter
+def shortnaturaltime(value):
+    c = datetime.datetime.now(datetime.timezone.utc)-value
+    if c.days:
+        return str(c.days) + '天之前'
+    elif c.seconds:
+        if c.seconds < 60:
+            return str(c.seconds) + '秒之前'
+        elif 60 <= c.seconds < 3600:
+            return str(c.seconds//60) + '分钟之前'
+        else:
+            return str(c.seconds//3600) + '小时之前'
+
